729cd33f6389a574294ca16094e05214a5b2ba18
Jaeman Kim <daga4242@gmail.com>
2024-01-03 03:55:48 +0900

    fix: check browser api

--- src/libraries/axios.ts
+++ src/libraries/axios.ts
@@ -2,19 +2,21 @@ import { ADDRESS_API_URL, API_URL } from '@/configurations';
 import { storage } from '@/utils/storage';
 import Axios, { InternalAxiosRequestConfig } from 'axios';
 
-const axiosRequestInterceptor = async (config: InternalAxiosRequestConfig) => {
+export const axiosRequestInterceptor = async (
+  config: InternalAxiosRequestConfig
+) => {
   if (config.headers) {
-    if (storage) {
-      const accessToken = storage.getValue('accessToken');
-      const refreshToken = storage.getValue('refreshToken');
-      const userUuid = storage.getValue('userUuid');
+    // if (checkWindow()) {
+    const accessToken = storage.getValue('accessToken');
+    const refreshToken = storage.getValue('refreshToken');
+    const userUuid = storage.getValue('userUuid');
 
-      if (accessToken && refreshToken && userUuid) {
-        config.headers.Authorization = 'Bearer ' + accessToken;
-        config.headers.refreshToken = refreshToken;
-        config.headers.userUuid = userUuid;
-      }
+    if (accessToken && refreshToken && userUuid) {
+      config.headers.Authorization = 'Bearer ' + accessToken;
+      config.headers.refreshToken = refreshToken;
+      config.headers.userUuid = userUuid;
     }
+    // }
     config.headers.Accept = 'application/json';
     config.headers['Content-Type'] = 'application/json';
   }
@@ -25,7 +27,7 @@ export const axios = Axios.create({
   baseURL: API_URL,
 });
 
-axios.interceptors.request.use(axiosRequestInterceptor);
+// axios.interceptors.request.use(axiosRequestInterceptor);
 
 export const addressRequest = Axios.create({
   baseURL: ADDRESS_API_URL,
