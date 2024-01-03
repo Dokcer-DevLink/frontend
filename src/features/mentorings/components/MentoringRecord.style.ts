b74f34756f6b255c36782a29e9b02489b90c7773
Jaeman Kim <daga4242@gmail.com>
2024-01-03 18:41:00 +0900

    feat: record mentoring

--- src/features/mentorings/components/MentoringRecord.style.ts
+++ src/features/mentorings/components/MentoringRecord.style.ts
@@ -17,10 +17,8 @@ export const FormInner = styled.div`
 `;
 
 export const Filename = styled.span`
-  width: 100%;
+  width: 400px;
 
-  display: flex;
-  align-items: center;
   padding: 5px;
 
   color: ${(props) => props.theme.schemes.light.onPrimaryContainer};
@@ -32,6 +30,10 @@ export const Filename = styled.span`
   font-weight: ${(props) => props.theme.styles.body.medium.fontWeight};
   line-height: ${(props) => props.theme.styles.body.medium.lineHeight}px;
   letter-spacing: ${(props) => props.theme.styles.body.medium.letterSpacing}px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export const Buttons = styled.div`
